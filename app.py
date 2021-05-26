0f2e7fca310ce17b9b5c9d128141e52b605868e0
ravikiranh0 <60516768+ravikiranh0@users.noreply.github.com>
2021-05-26 11:23:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,27 @@
 import requests
 import streamlit as st
 import joblib
+import time
 st.set_option( 'deprecation.showfileUploaderEncoding', False)
 st.title("COVID-19 Question Answering Webapp")
 st.text("Get accurate information regargding coronavirus!")
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model=joblib.load('bert_qa_custom.joblib')
-    return model
+# @st.cache(allow_output_mutation=True)
+# def load_model():
+#     model=joblib.load('bert_qa_custom.joblib')
+#     return model
 
-with st.spinner('Loading Model Into Memory....'):
-    model = load_model()
+# with st.spinner('Loading Model Into Memory....'):
+#     model = load_model()
 
 text = st.text_input('Enter your questions here..')
 
 if text:
     st.write("Response :")
     with st.spinner( 'Searching for answers.....'):
-        prediction = model.predict(text)
-        st.write('answer: {}'.format(prediction[0]))
-        st.write('title: {}'.format(prediction[1]))
-        st.write('paragraph: {}'.format(prediction[2]))
+        time.sleep(15)
+        st.balloons()
+        st.write('answer: {}'.format("")
+        st.write('title: {}'.format("")
+        st.write('paragraph: {}'.format("")
     st.write("")
