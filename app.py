a6220764d28a5749f86268fcbe943f364b5c692f
ravikiranh0 <60516768+ravikiranh0@users.noreply.github.com>
2021-05-26 11:41:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,22 +7,27 @@ import time
 st.set_option( 'deprecation.showfileUploaderEncoding', False)
 st.title("COVID-19 Question Answering Webapp")
 st.text("Get accurate information regargding coronavirus!")
-
+st.spinner('Loading Model Into Memory....')
 # @st.cache(allow_output_mutation=True)
 # def load_model():
 #     model=joblib.load('bert_qa_custom.joblib')
 #     return model
 
-# with st.spinner('Loading Model Into Memory....'):
+
+    # print("loading")
 #     model = load_model()
 
 text = st.text_input('Enter your questions here..')
-
+# with st.spinner('Searching'):
+#     time.sleep(5)
+#     st.balloons()
 if text:
     st.write("Response :")
     with st.spinner( 'Searching for answers.....'):
         time.sleep(15)
         st.balloons()
+        # prediction = model.predict(text)
         st.write('answer: {}'.format(""))
         st.write('title: {}'.format(""))
-        st.write('paragraph: {}'.format("")
+        st.write('paragraph: {}'.format(""))
+    st.write("")
